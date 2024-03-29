5b0d7191fb5e48e8c1286b614086bbdb9bfcc5c5
jinhojang6 <jj142321@proton.me>
2024-03-29 19:01:00 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+
+import styles from "./index.module.css";
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">Hyperion</div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      Hyperion
+    </Layout>
+  );
+}
